01a1d907fd4c0edac54b1f6d1cc801c8cd15f30d
PanaW <pana.wang@outlook.com>
2022-06-21 23:06:27 +0800

    add support for contract verify

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { extendEnvironment } from "hardhat/config";
-import { lazyObject } from "hardhat/plugins";
+import { extendEnvironment, task, types } from "hardhat/config";
 import { NomicLabsHardhatPluginError } from "hardhat/plugins";
+import { lazyObject } from "hardhat/plugins";
 import { 
   Conflux, 
   format,
@@ -17,8 +17,78 @@ import {
   getContractFactory,
   getSigners,
 } from './helpers';
-
+import {
+  verifySourceCode
+} from './scan-cli'
 import "./type-extensions";
+
+const TASK_FLATTEN_GET_FLATTENED_SOURCE = "flatten:get-flattened-sources";
+
+task("verifyCfxContract", "Verify a Conflux contract")
+  .addPositionalParam(
+    "contractName",
+    "Contract name to verify",
+    undefined,
+    types.string
+  )
+  .addPositionalParam(
+    "address",
+    "Base32 address of the smart contract to verify",
+    undefined,
+    types.string
+  )
+  .addOptionalParam(
+    "compilerVersion",
+    "Compiler version",
+    undefined,
+    types.string,
+  )
+  .addOptionalParam(
+    "constructorArgs",
+    "File path to a javascript module that exports the list of arguments.",
+    undefined,
+    types.inputFile
+  )
+  .setAction(verifyConflux);
+
+// @ts-ignore
+async function verifyConflux(args, hre) {
+  const {
+    contractName,
+    address: contractaddress,
+    compilerVersion,
+  } = args;
+  // TODO: support constructor parameters
+  
+  // check address
+  if (!address.isValidCfxAddress(contractaddress)) {
+    throw new NomicLabsHardhatPluginError('hardhat-conflux', 'Invalid Conflux base32 contract address.');
+  }
+  const addressInfo = address.decodeCfxAddress(contractaddress);
+  if (addressInfo.type !== 'contract') {
+    throw new NomicLabsHardhatPluginError('hardhat-conflux', 'This is not a contract address.');
+  }
+
+  try {
+    const compilerversion = compilerVersion ? compilerVersion : hre.userConfig.solidity;
+    const artifact = await hre.artifacts.readArtifact(contractName);
+    const sourceCode = await hre.run(TASK_FLATTEN_GET_FLATTENED_SOURCE, {files: [artifact.sourceName]});
+    const result = await verifySourceCode({
+      networkId: addressInfo.netId,  //hre.network.config.chainId,
+      contractaddress,
+      sourceCode,
+      contractname: contractName,
+      compilerversion,
+    });
+    if (result.code == 0) {
+      console.log(`Verify success: ${contractName}`);
+    } else {
+      console.log(`Verify failed: ${result.message}`);
+    }
+  } catch(e: any) {
+    console.log(`Verify failed: ${e.message}`);
+  }
+}
 
 extendEnvironment((hre) => {
 
@@ -55,7 +125,7 @@ extendEnvironment((hre) => {
       }
     } else {
       // TODO: HD wallet
-      throw new NomicLabsHardhatPluginError('hardhat-conflux', 'HD wallet is not supported yet.');
+      // throw new NomicLabsHardhatPluginError('hardhat-conflux', 'HD wallet is not supported yet.');
     }
     // @ts-ignore
     conflux.getContractAt = getContractAt.bind(null, hre);
